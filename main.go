40eca0e91e4d134f15d1d9a385915f4df1669c91
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-07-05 17:20:42 +0200

    version bump

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ var (
 )
 
 const (
-	VERSION                          = "1.29.3"
+	VERSION                          = "1.30"
 	DATABASE_DIR                     = "database"
 	RELEASE_URL                      = "https://github.com/Seklfreak/discord-image-downloader-go/releases/latest"
 	RELEASE_API_URL                  = "https://api.github.com/repos/Seklfreak/discord-image-downloader-go/releases/latest"
